b3e8d5c944b0521e784002137ff6a5a6aaf5bd5c
Aditya Musale <adityamusale4477@gmail.com>
2022-02-27 13:54:52 +0530

    created header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
-import Logo from '../../assets/logo.png'
+import Icon from '../../assets/sprite.svg'
 
 const Header = () => {
   return (
     <div className='header' >
       <div className="logo">
-        <img src={Logo} alt="trillo logo" />
+        <img src={require('../../assets/logo.png')} alt="trillo logo" className='logo__image' />
       </div>
 
       <form action="#" className="search">
-        <input type="text" className="search__box" />
+        <input type="text" className="search__box" placeholder='Search...' />
         <button className="search__button">
           <svg className="search__icon">
-            <use xlinkHref='../../assets/sprite.svg#icon-magnifying-glass'></use>
+            <use xlinkHref={`${Icon}#icon-magnifying-glass`}></use>
           </svg>
         </button>
       </form>
 
-      <div className="user-nav__icon-box">
-        <svg className='user-nav__icon'>
-          <use xlinkHref='../../assets/sprite.svg#icon-bookmark'></use>
-        </svg>
-        <span className="user-nav__notification">7</span>
-      </div>
+      <nav className="user-nav">
+        <div className="user-nav__icon-box">
+          <svg className='user-nav__icon'>
+            <use xlinkHref={`${Icon}#icon-bookmark`}></use>
+          </svg>
+          <span className="user-nav__notification">7</span>
+        </div>
 
-      <div className="icon-box">
-        <svg className='user-nav__icon'>
-          <use xlinkHref='../../assets/sprite.svg#icon-chat'></use>
-        </svg>
-        <span className="user-nav__notification">13</span>
-      </div>
+        <div className="user-nav__icon-box">
+          <svg className='user-nav__icon'>
+            <use xlinkHref={`${Icon}#icon-chat`}></use>
+          </svg>
+          <span className="user-nav__notification">13</span>
+        </div>
 
-      <div className="user-nav__user">
-        <svg className='user-nav__icon'>
-          <use xlinkHref='../../assets/sprite.svg#icon-bookmark'></use>
-        </svg>
-        <span className="user-nav__username">aditya</span>
-      </div>
+        <div className="user-nav__user">
+          <img src={require('../../assets/user.jpg')} alt="user" className='user-nav__user--img' />
+          <span className="user-nav__user--username">Aditya</span>
+        </div>
+      </nav>
     </div>
   )
 }
